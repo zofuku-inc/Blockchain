c849195dd14b747814cc5e639ad04fbdeb0811b0
Your Name <you@example.com>
2020-09-17 10:23:37 -0700

    tried cors config

--- index.js
+++ index.js
@@ -4,31 +4,23 @@ const formData = require('express-form-data')
 const cors = require('cors');
 require('dotenv').config();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://store.spaceincome.jp');
-    next();
-  });
-app.use(express.json())
-app.use(formData.parse())
-app.use(cors({
-    origin: 'https://store.spaceincome.jp',
-    allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "Access-Control-Allow-Methods",
-        "Access-Control-Request-Headers"
-      ],
-    credentials: true,
-    enablePreflight: true
-}))
-app.set('trust proxy', 1)
-
 const imageRoute = require('./api/routes/image');
 const invoiceRoute = require('./api/routes/invoices');
 const sendInvoiceRoute = require('./api/routes/sendInvoice');
 const stripeCheckOutRoute = require('./api/routes/payment')
 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://store.spaceincome.jp');
+    next();
+  });
+app.set('trust proxy', 1)
+app.use(express.json())
+app.use(formData.parse())
+app.use(cors({
+    origin: 'https://store.spaceincome.jp',
+    credentials: true
+}))
 app.use('/images', imageRoute);
 app.use('/invoices', invoiceRoute);
 app.use('/sendInvoice', sendInvoiceRoute);
